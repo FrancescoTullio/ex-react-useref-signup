4d23f90740ccbd78d974a45191b7b5a99a33fa9f
FrancescoTullio <francescotullio1992@gmail.com>
2025-04-05 20:06:10 +0200

    feat: controlli dinamici

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,33 @@ function App() {
     biograpy: ""
   };
 
+  const validationObj = {
+    userVal: false,
+    passVal: false,
+    bioVal: false
+  }
+
+  const specialChars = [
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+    "[", "]", "{", "}", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/",
+    "`", "~", "£", `"`
+  ];
+
+  const letters = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+  ];
+
+  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
   const [formData, setFormData] = useState(defaultForm);
+  const [val, setVal] = useState(validationObj)
+
+
+
 
   const handleChange = (event) => {
     const newObj = {
@@ -20,18 +46,100 @@ function App() {
       [event.target.name]: event.target.value
     };
 
+
+
+
+    if (event.target.name === "userName") {
+
+      if (newObj.userName.trim().length > 5) {
+        const iter = newObj.userName.trim()
+        const newVal = {
+              ...val,
+              userVal: false
+            }
+            let count = 0
+        for (const char of iter) {
+          if (specialChars.includes(char) || char === " ") {
+            count++
+          }
+        }
+        if(count === 0) {
+          newVal.userVal = true
+        }
+        setVal(newVal)
+      } else {
+
+
+        const newVal = {
+          ...val,
+          userVal: false
+        }
+        setVal(newVal)
+      }
+    }
+
+    if (event.target.name === "password") {
+      if (newObj.password.trim().length > 8) {
+        const iter = newObj.password
+        const newVal = {
+          ...val,
+          passVal: false
+        }
+        let lettere = 0
+        let simboli = 0
+        let numeri = 0
+        for (const char of iter) {
+
+
+          if (digits.includes(char)) {
+            numeri++
+          }
+          if (letters.includes(char)) {
+            lettere++
+          }
+          if (specialChars.includes(char)) {
+            simboli++
+          }
+        }
+
+        if (lettere > 0 && simboli > 0 && numeri > 0) {
+          newVal.passVal = true
+        }
+
+        setVal(newVal)
+
+      } else {
+        const newVal = {
+          ...val,
+          passVal: false
+        }
+        setVal(newVal)
+      }
+    }
+
+    if (event.target.name === "biograpy"){
+      const newVal = {
+        ...val,
+        bioVal: false
+      }
+      if(newObj.biograpy.trim().length >= 100 & newObj.biograpy.trim().length <= 1000){
+        newVal.bioVal = true
+      }
+      setVal(newVal)
+    }
+
     setFormData(newObj);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(formData.fullName.trim() && formData.userName.trim() && formData.password.trim() && formData.specializzation && formData.anzianity >= 0 && formData.biograpy){
+    if (formData.fullName.trim() && formData.userName.trim() && formData.password.trim() && formData.specializzation && formData.anzianity >= 0 && formData.biograpy) {
       console.log(formData);
     } else {
       console.log("qualcosa non va nel form");
-      
+
     }
-    
+
   };
 
   return (
@@ -51,6 +159,12 @@ function App() {
           </div>
           <div className="mb-3">
             <label className="form-label">Username</label>
+            {
+              (!val.userVal && formData.userName) && <p className="text-danger">User non conforme</p>
+            }
+            {
+              val.userVal && <p className="text-success">user ok </p>
+            }
             <input
               type="text"
               className="form-control"
@@ -61,6 +175,12 @@ function App() {
           </div>
           <div className="mb-3">
             <label className="form-label">Password</label>
+            {
+              (!val.passVal && formData.password) && <p className="text-danger">Password non conforme</p>
+            }
+            {
+              val.passVal && <p className="text-success">Pass ok</p>
+            }
             <input
               type="password"
               className="form-control"
@@ -97,6 +217,12 @@ function App() {
           </div>
           <div className="mb-3">
             <label className="form-label">Biografia</label>
+            {
+              (!val.bioVal && formData.biograpy) && <p className="text-danger">Biografia non conforme</p>
+            }
+            {
+              val.bioVal && <p className="text-success">bio ok</p>
+            }
             <textarea
               name="biograpy"
               className="form-control"
