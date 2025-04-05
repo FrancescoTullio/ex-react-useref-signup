e067c5a62c812b0da28a56490428e57a7d78ab74
FrancescoTullio <francescotullio1992@gmail.com>
2025-04-05 18:18:34 +0200

    feat: prima milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,117 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
+  const optionSpec = ["Full Stack", "Frontend", "Backend"];
 
-  const optionSpec = ["Full Stack", "Frontend", "Backend"]
-
-  const dafaulForm = {
+  const defaultForm = {
     fullName: "",
     userName: "",
     password: "",
     specializzation: "",
     anzianity: "",
     biograpy: ""
-  }
+  };
 
-  const [formData, setFormData] = useState(dafaulForm)
+  const [formData, setFormData] = useState(defaultForm);
 
   const handleChange = (event) => {
     const newObj = {
       ...formData,
       [event.target.name]: event.target.value
+    };
+
+    setFormData(newObj);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(formData.fullName.trim() && formData.userName.trim() && formData.password.trim() && formData.specializzation && formData.anzianity >= 0 && formData.biograpy){
+      console.log(formData);
+    } else {
+      console.log("qualcosa non va nel form");
+      
     }
-
-    setFormData(newObj)
-  }
     
-
-  
+  };
 
   return (
     <>
       <div className="container">
-        <h1 className="text-center">iscritivi alla piattaforma</h1>
-        <form action="" className="form-control">
+        <h1 className="text-center">Iscriviti alla piattaforma</h1>
+        <form className="form-control" onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label className="form-contro">Nome</label>
-            <input type="text" className="form-control" name="fullName" value={formData.fullName} onChange={(event) => handleChange(event)} />
+            <label className="form-label">Nome</label>
+            <input
+              type="text"
+              className="form-control"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+            />
           </div>
           <div className="mb-3">
-            <label className="form-contro">Username</label>
-            <input type="text" className="form-control" name="userName" value={formData.userName} onChange={(event) => handleChange(event)} />
+            <label className="form-label">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              name="userName"
+              value={formData.userName}
+              onChange={handleChange}
+            />
           </div>
           <div className="mb-3">
-            <label className="form-contro">Password</label>
-            <input type="password" className="form-control" name="password" value={formData.password} onChange={(event) => handleChange(event)} />
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </div>
           <div className="mb-3">
-            <select className="form-select" name="specializzation" value={formData.specializzation} onChange={handleChange}>
-              <option value="">seleziona una specializzazione</option>
-              {
-                optionSpec.map((curElem, i) => <option value={curElem} key={i}>{curElem}</option>)
-              }
+            <label className="form-label">Specializzazione</label>
+            <select
+              className="form-select"
+              name="specializzation"
+              value={formData.specializzation}
+              onChange={handleChange}
+            >
+              <option value="">Seleziona una specializzazione</option>
+              {optionSpec.map((curElem, i) => (
+                <option value={curElem} key={i}>
+                  {curElem}
+                </option>
+              ))}
             </select>
           </div>
           <div className="mb-3">
-            <label className="form-contro">Anni di Esperienza</label>
-            <input type="number" className="form-control" name="anzianity" value={formData.anzianity} onChange={(event) => handleChange(event)} />
+            <label className="form-label">Anni di Esperienza</label>
+            <input
+              type="number"
+              className="form-control"
+              name="anzianity"
+              value={formData.anzianity}
+              onChange={handleChange}
+            />
           </div>
-
           <div className="mb-3">
-            <label htmlFor="" className="form-data">
-              <textarea name="biograpy" className="form-data" rows="5" value={formData.biograpy} onChange={handleChange}></textarea>
-            </label>
+            <label className="form-label">Biografia</label>
+            <textarea
+              name="biograpy"
+              className="form-control"
+              rows="5"
+              value={formData.biograpy}
+              onChange={handleChange}
+            ></textarea>
           </div>
-
-
-
-          <button className="btn btn-primary" type="submite">iscrciviti</button>
+          <button className="btn btn-primary" type="submit">
+            Iscriviti
+          </button>
         </form>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
